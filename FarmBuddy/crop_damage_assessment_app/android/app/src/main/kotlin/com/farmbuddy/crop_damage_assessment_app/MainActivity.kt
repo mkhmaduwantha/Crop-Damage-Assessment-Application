a6ddde8d2602bfe3d1a9c4408dad774eeef637e9
hemakaraveenhansika <hemakaraveenhansika@gmail.com>
2022-02-27 19:25:22 +0530

    fix project initiation

--- FarmBuddy/crop_damage_assessment_app/android/app/src/main/kotlin/com/farmbuddy/crop_damage_assessment_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.farmbuddy.crop_damage_assessment_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- FarmBuddy/crop_damage_assessment_app/ios/Flutter/flutter_export_environment.sh
+++ FarmBuddy/crop_damage_assessment_app/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\Software_Installed\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Projects\FarmBuddy\crop_damage_assessment_app"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Academic\Sem 8\Mobile Computing\Project\FarmBuddy\Crop-Damage-Assessment-Application\FarmBuddy\crop_damage_assessment_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
